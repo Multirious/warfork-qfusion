771b9f31c33115363f6b2ed7072f9ccd7a82368c
Multirious <multirious@outlook.com>
2026-08-05 14:59:28 +0700

    Revert "Make package_assets in linux deterministic"
    This reverts commit 757e7f5867107ee2e37a2bb991be9a5e4b38db1e.

--- source/package_assets.cmake
+++ source/package_assets.cmake
@@ -30,39 +30,12 @@ if (${CMAKE_SYSTEM_NAME} MATCHES "Windows")
     message(WARNING "OpenAL DLL not found: ${OPENAL_DLL_SRC}")
   endif()
 else()
-  execute_process (COMMAND bash -c "
-    mkdir -p \"${BIN_DIR}/basewf\"
-
-    files=(
-      data0_000_21
-      data0_000_21pure
-      data0_21
-      data0_21pure
-      data1_21pure
-    )
-
-    # Sanity check zip version
-    zip --version
-
-    echo \"Preparing assets...\"
-    # Preparing assets for deterministic hash result
-    tempdir=$(mktemp -d)
-    for p in \"\${files[@]}\"; do
-      cp -r \"${ASSET_ROOT}/$p\" $tempdir
-    done
-    find $tempdir -exec touch -d '1985-10-21 09:00:00' {} \;
-    find $tempdir -type f -exec chmod 644 {} \;
-
-    echo \"Zipping assets...\"
-    for p in \"\${files[@]}\"; do
-      # Use sort to present file deterministically to zip
-      # Force timezone to UTC to avoid confusion
-      cd \"$tempdir/$p\"
-      find . -type f | sort | TZ=UTC zip -X \"${BIN_DIR}/basewf/$p.pk3\" -@ 
-    done
-
-    rm -r $tempdir
-  ")
+  execute_process (COMMAND mkdir -p ${BIN_DIR}/basewf)
+  execute_process (COMMAND bash -c "cd ${ASSET_ROOT}/data0_000_21/ && zip -r ${BIN_DIR}/basewf/data0_000_21.pk3 *")
+  execute_process (COMMAND bash -c "cd ${ASSET_ROOT}/data0_000_21pure/ && zip -r ${BIN_DIR}/basewf/data0_000_21pure.pk3 *")
+  execute_process (COMMAND bash -c "cd ${ASSET_ROOT}/data0_21/ && zip -r ${BIN_DIR}/basewf/data0_21.pk3 *")
+  execute_process (COMMAND bash -c "cd ${ASSET_ROOT}/data0_21pure/ && zip -r ${BIN_DIR}/basewf/data0_21pure.pk3 *")
+  execute_process (COMMAND bash -c "cd ${ASSET_ROOT}/data1_21pure/ && zip -r ${BIN_DIR}/basewf/data1_21pure.pk3 *")
 endif()
 
 file(COPY ${ASSET_ROOT}/profiles  DESTINATION ${BIN_DIR}/basewf)
